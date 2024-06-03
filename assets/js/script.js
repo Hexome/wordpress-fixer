0e3f83dc0bd8dbcc5f99665b7384bfc67552f47e
Juan manuel Pedro Villalba <xomie_01@hotmail.com>
2024-06-02 22:39:27 -0300

    Update script.js
    * Version:         0.0.2

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,7 +15,7 @@ class StyleController {
 }
 
 function onDOMLoaded() {
-    const controller = new StyleController('.wp-link, .author-link, div.rtc-contact-widget-wrap > ul.contact-list > li > a, section#rtc_social_links-2 > ul.social-networks > li.rtc-social-icon-wrap > a, div.container > div.copyright > span.copyright-text > a', 5000); 
+    const controller = new StyleController('.job_listings, .content, .wp-link, .author-link, div.rtc-contact-widget-wrap > ul.contact-list > li > a, section#rtc_social_links-2 > ul.social-networks > li.rtc-social-icon-wrap > a, div.container > div.copyright > span.copyright-text > a', 5000); 
     controller.changeStyles();
 }
 
