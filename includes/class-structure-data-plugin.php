2bb316544f06dd4bd0f80b8f04c6fd2b92898311
Juan manuel Pedro Villalba <xomie_01@hotmail.com>
2024-06-04 00:35:18 -0300

    Update class-structure-data-plugin.php

--- includes/class-structure-data-plugin.php
+++ includes/class-structure-data-plugin.php
@@ -6,7 +6,7 @@
  * Author:          Villalba Juan Manuel Pedro
  * Author URI:      https://hexome.cloud
  * Text Domain:     wordpress-fixer
- * Version:         0.0.6
+ * Version:         0.0.7
  *
  * @package         Hexome_Fixer
  */
@@ -16,16 +16,34 @@ if (!defined('ABSPATH')) exit;
 
 class Structure_Data_Plugin {
 
-    public function __construct() {
-        add_filter('wpseo_json_ld_output', [$this, 'alr_structured_data']);
+     public function __construct() {
+        if (class_exists('WPSEO_Frontend')) {
+            add_filter('wpseo_json_ld_output', [$this, 'alr_structured_data_yoast']);
+        }
+
+        if (defined('RANK_MATH_VERSION')) {
+            add_filter('rank_math/json_ld', [$this, 'alr_structured_data_rankmath'], 10, 2);
+        }
     }
 
-    public function alr_structured_data($data) {
+    public function alr_structured_data_yoast($data) {
         if (isset($data['@type']) && $data['@type'] === 'JobPosting') {
             if (!isset($data['applicantLocationRequirements'])) {
                 $data['applicantLocationRequirements'] = [
                     '@type' => 'Country',
-                    'name' => 'US' 
+                    'name' => 'US'
+                ];
+            }
+        }
+        return $data;
+    }
+
+    public function alr_structured_data_rankmath($data, $jsonld) {
+        if (isset($data['@type']) && $data['@type'] === 'JobPosting') {
+            if (!isset($data['applicantLocationRequirements'])) {
+                $data['applicantLocationRequirements'] = [
+                    '@type' => 'Country',
+                    'name' => 'US'
                 ];
             }
         }
