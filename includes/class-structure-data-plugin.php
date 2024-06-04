beb4f406765d60934cef05d1e8e8a6351910d876
Juan manuel Pedro Villalba <xomie_01@hotmail.com>
2024-06-04 00:58:11 -0300

    Update class-structure-data-plugin.php
    * Version:         0.0.8

--- includes/class-structure-data-plugin.php
+++ includes/class-structure-data-plugin.php
@@ -6,7 +6,7 @@
  * Author:          Villalba Juan Manuel Pedro
  * Author URI:      https://hexome.cloud
  * Text Domain:     wordpress-fixer
- * Version:         0.0.7
+ * Version:         0.0.8
  *
  * @package         Hexome_Fixer
  */
@@ -17,15 +17,40 @@ if (!defined('ABSPATH')) exit;
 class Structure_Data_Plugin {
 
      public function __construct() {
-        if (class_exists('WPSEO_Frontend')) {
-            add_filter('wpseo_json_ld_output', [$this, 'alr_structured_data_yoast']);
-        }
+             if (class_exists('WPSEO_Frontend')) {
+                 add_filter('wpseo_json_ld_output', [$this, 'alr_structured_data_yoast']);
+             }
+     
+             if (defined('RANK_MATH_VERSION')) {
+                 add_filter('rank_math/json_ld', [$this, 'alr_structured_data_rankmath'], 10, 2);
+             }
 
-        if (defined('RANK_MATH_VERSION')) {
-            add_filter('rank_math/json_ld', [$this, 'alr_structured_data_rankmath'], 10, 2);
-        }
+             add_action('wp_footer', [$this, 'alr_wp_job_manager_json_ld'], 100);
+         }
+
+    
+    
+
+    public function alr_wp_job_manager_json_ld() {
+        ob_start([$this, 'alr_json_ld_output']);
     }
 
+    public function alr_json_ld_output($buffer) {
+        $pattern = '/<script type="application\/ld\+json">(.*?)<\/script>/s';
+        $buffer = preg_replace_callback($pattern, function($matches) {
+            $json = json_decode($matches[1], true);
+            if (isset($json['@type']) && $json['@type'] === 'JobPosting') {
+                if (!isset($json['applicantLocationRequirements'])) {
+                    $json['applicantLocationRequirements'] = [
+                        '@type' => 'Country',
+                        'name' => 'US' // Puedes cambiar "US" por el país que desees
+                    ];
+                }
+            }
+            return '<script type="application/ld+json">' . json_encode($json) . '</script>';
+        }, $buffer);
+        return $buffer;
+    }
     public function alr_structured_data_yoast($data) {
         if (isset($data['@type']) && $data['@type'] === 'JobPosting') {
             if (!isset($data['applicantLocationRequirements'])) {
