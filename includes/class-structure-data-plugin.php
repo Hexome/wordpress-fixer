84781af32087ef907b0e4f4a7395db661e41dea7
Juan manuel Pedro Villalba <xomie_01@hotmail.com>
2024-06-05 17:05:14 -0300

    Update class-structure-data-plugin.php
    Version:         0.0.10

--- includes/class-structure-data-plugin.php
+++ includes/class-structure-data-plugin.php
@@ -6,7 +6,7 @@
  * Author:          Villalba Juan Manuel Pedro
  * Author URI:      https://hexome.cloud
  * Text Domain:     wordpress-fixer
- * Version:         0.0.9
+ * Version:         0.0.10
  *
  * @package         Hexome_Fixer
  */
@@ -16,43 +16,31 @@ if (!defined('ABSPATH')) exit;
 
 class Structure_Data_Plugin {
 
-     public function __construct() {
-          
-             if (class_exists('WPSEO_Frontend')) {
-                 add_filter('wpseo_json_ld_output', [$this, 'alr_structured_data_yoast']);
-             }
-     
-             if (defined('RANK_MATH_VERSION')) {
-                 add_filter('rank_math/json_ld', [$this, 'alr_structured_data_rankmath'], 10, 2);
-             }
-          
-             if (defined('JOB_MANAGER_VERSION')) {
-                  add_action('wp_footer', [$this, 'alr_wp_job_manager_json_ld'], 100);
-                  add_filter('wpjm_get_job_listing_structured_data', [$this, 'alr_wpjm_structured_data'], 10, 2); 
-             }
-             
-             
+    public function __construct() {
+        if (class_exists('WPSEO_Frontend')) {
+            add_filter('wpseo_json_ld_output', [$this, 'alr_structured_data']);
+        }
+
+        if (defined('RANK_MATH_VERSION')) {
+            add_filter('rank_math/json_ld', [$this, 'alr_structured_data'], 10, 2);
+        }
+
+        if (defined('JOB_MANAGER_VERSION')) {
+            add_action('wp_footer', [$this, 'alr_wp_job_manager_json_ld'], 100);
+            add_filter('wpjm_get_job_listing_structured_data', [$this, 'alr_wpjm_structured_data'], 10, 2); 
+        }
     }
-    public function alr_structured_data_yoast($data) {
+
+    public function alr_structured_data($data) {
         if (isset($data['@type']) && $data['@type'] === 'JobPosting') {
-            if (!isset($data['applicantLocationRequirements'])) {
-                $data['applicantLocationRequirements'] = [
-                    '@type' => 'Country',
-                    'name' => 'US' 
-                ];
-            }
+            $data = $this->add_missing_fields($data);
         }
         return $data;
     }
 
     public function alr_structured_data_rankmath($data, $jsonld) {
         if (isset($data['@type']) && $data['@type'] === 'JobPosting') {
-            if (!isset($data['applicantLocationRequirements'])) {
-                $data['applicantLocationRequirements'] = [
-                    '@type' => 'Country',
-                    'name' => 'US' 
-                ];
-            }
+            $data = $this->add_missing_fields($data);
         }
         return $data;
     }
@@ -66,12 +54,7 @@ class Structure_Data_Plugin {
         $buffer = preg_replace_callback($pattern, function($matches) {
             $json = json_decode($matches[1], true);
             if (isset($json['@type']) && $json['@type'] === 'JobPosting') {
-                if (!isset($json['applicantLocationRequirements'])) {
-                    $json['applicantLocationRequirements'] = [
-                        '@type' => 'Country',
-                        'name' => 'US' 
-                    ];
-                }
+                $json = $this->add_missing_fields($json);
             }
             return '<script type="application/ld+json">' . json_encode($json) . '</script>';
         }, $buffer);
@@ -80,13 +63,23 @@ class Structure_Data_Plugin {
 
     public function alr_wpjm_structured_data($data, $post) {
         if (isset($data['@type']) && $data['@type'] === 'JobPosting') {
-            if (!isset($data['applicantLocationRequirements'])) {
-                $data['applicantLocationRequirements'] = [
-                    '@type' => 'Country',
-                    'name' => 'US' 
-                ];
-            }
+            $data = $this->add_missing_fields($data);
         }
+        return $data;
+    }
+
+    private function add_missing_fields($data) {
+        if (!isset($data['applicantLocationRequirements'])) {
+            $data['applicantLocationRequirements'] = [
+                '@type' => 'Country',
+                'name' => 'US' 
+            ];
+        }
+
+        if (!isset($data['hiringOrganization']['name'])) {
+            $data['hiringOrganization']['name'] = 'Private Business'; 
+        }
+
         return $data;
     }
 }
