5cbbcee17e0f2e6debaeb5c1c0ddcfc3acdc4221
Juan manuel Pedro Villalba <xomie_01@hotmail.com>
2024-06-05 21:07:42 -0300

    Update wordpress-fixer.php
    0.0.19

--- wordpress-fixer.php
+++ wordpress-fixer.php
@@ -7,12 +7,12 @@
  * Author URI:      https://hexome.cloud
  * Text Domain:     wordpress-fixer
  * Domain Path:     /languages
- * Version:         0.0.17
+ * Version:         0.0.19
  *
  * @package         Hexome_Fixer
  */
 
-!define('WPFIXER_CURRENT_VERSION', '0.0.17');
+!define('WPFIXER_CURRENT_VERSION', '0.0.19');
 !define('WPFIXER_GITHUB_USER', 'Hexome');
 !define('WPFIXER_GITHUB_REPO', 'wordpress-fixer');
 !define('WPFIXER_PLUGIN_FILE', plugin_dir_path( __FILE__ ));
